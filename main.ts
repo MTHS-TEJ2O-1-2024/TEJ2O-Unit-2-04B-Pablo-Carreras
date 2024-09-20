349e09fd0ed12b8fb15fd7cbe68519661f7f6c0e
Pablo-Carreras <pablo.carreras-canton@mths.ca>
2024-09-20 10:54:44 -0400

    added showstring to code

--- main.ts
+++ main.ts
@@ -5,13 +5,14 @@
  * This program says the temperature from the microbit
 */
 
-// says the temperature f the microbit 
+// says the temperature from the microbit 
 let currentTemperature = input.temperature()
 
 basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 
 input.onButtonPressed(Button.A, function () {
+basic.showString('The temperature is')
 currentTemperature = input.temperature()
 basic.showNumber(currentTemperature)
 })
